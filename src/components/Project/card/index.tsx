240393355a71b1470aab0701894cc6f88268e293
AvinChailiSalim <avinchailisalim@gmail.com>
2023-08-02 13:24:54 +0700

    editing cards

--- src/components/Project/card/index.tsx
+++ src/components/Project/card/index.tsx
@@ -59,12 +59,12 @@ export default function Card({type, children}: Props) {
                      <div className="grid gap-4 grid-cols-5 grid-rows-1">
                     {arrCard.map((card, index) => (
                     <div className="w-[197px] h-[197px] p-3 flex-col justify-center items-center 
-                    gap-[14px] rounded-lg bg-primary-600 text-white
+                    gap-[14px] rounded-lg bg-primary-600 text-white group
                     hover:bg-primary-700 hover:scale-125">
-                        <div className="flex items-center justify-center">
+                        <div className="flex items-center justify-center group-hover:scale-125">
                             <img src={Sarjana} width='71px' height='71px'/>
                         </div>
-                        <div>
+                        <div className="group-hover:translate-y-4">
                             <div>{card.type}</div>
                             <div key={index} className="font-bold line-clamp-3">{card.desc}</div>
                         </div>    
